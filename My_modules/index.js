3a0f91ef88c6f46d52928cc4cadb1acbfecbfca5
shota <1014.onno@gmail.com>
2025-08-04 23:21:31 +0900

    index.jsの修正(エラー解消)

--- My_modules/index.js
+++ My_modules/index.js
@@ -1,7 +1,10 @@
-const arithmetic = require('./arithmetic.js'); // arithmetic.jsから関数をインポートする
-const string_concat = require('./string_concat.js'); // string_concat.jsから関数
+// My_modules/index.js
+const arithmetic    = require('./arithmetic');
+const string_concat = require('./string_concat');
 
-module.export = {
-    arithmetic,
-    string_concat
+module.exports = {
+  arithmetic,
+  string_concat
 };
+// このファイルは、他のモジュールからarithmeticとstring_concatをまとめてインポートできるようにするためのエントリーポイントです。
+// これにより、他のファイルでは以下のようにインポートできます
