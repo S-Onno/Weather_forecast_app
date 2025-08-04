21da4f54018878e664fa975df2c483b43c6878dc
shota <1014.onno@gmail.com>
2025-08-04 23:15:06 +0900

    スペルミス修正

---
+++ My_modules/index.js
@@ -0,0 +1,7 @@
+const arithmetic = require('./arithmetic.js'); // arithmetic.jsから関数をインポートする
+const string_concat = require('./string_concat.js'); // string_concat.jsから関数
+
+module.export = {
+    arithmetic,
+    string_concat
+};
